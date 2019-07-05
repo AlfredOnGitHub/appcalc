37431bd05bf6ca7aee2041a3b8a9ca19a2a5fcad
Alfredo Montalvo <yo@localhost.localdomain>
2019-07-05 13:12:11 +0200

    Calculadora V.0.2

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,25 +3,41 @@ print("Bienvenido a la calculadora. Introduce el dígito de lo que quieres hacer
 while True:
     opcion = int(input("MENU: Introduzca opcion: "))
     if opcion == 1:
-        a = int(input("SUMA: Introduce dígito: "))
-        b = int(input("SUMA: Introduce otro dígito: "))
-        suma = a + b
-        print(suma)
+        try:
+            a = int(input("SUMA: Introduce dígito: "))
+            b = int(input("SUMA: Introduce otro dígito: "))
+        except ValueError:
+            print("Introduce un dígito, por favor. (Usted ha vuelto al menú.)")
+        else:
+            suma = a + b
+            print(suma)
     elif opcion == 2:
-        a = int(input("RESTA: Introduce dígito: "))
-        b = int(input("RESTA: Introduce otro dígito: "))
-        resta = a - b
-        print(resta)
+        try:
+            a = int(input("RESTA: Introduce dígito: "))
+            b = int(input("RESTA: Introduce otro dígito: "))
+        except ValueError:
+            print("Introduce un dígito, por favor. (Usted ha vuelto al menú.)")
+        else:
+            resta = a - b
+            print(resta)
     elif opcion == 3:
-        a = int(input("MULTIPLICACIÓN: Introduce dígito: "))
-        b = int(input("MULTIPLICACIÓN: Introduce otro dígito: "))
-        mult = a*b
-        print(mult)
+        try:
+            a = int(input("MULTIPLICACIÓN: Introduce dígito: "))
+            b = int(input("MULTIPLICACIÓN: Introduce otro dígito: "))
+        except ValueError:
+            print("Introduce un dígito, por favor. (Usted ha vuelto al menú.)")
+        else:
+            mult = a*b
+            print(mult)
     if opcion == 4:
-        a = int(input("DIVISIÓN: Introduce dígito: "))
-        b = int(input("DIVISIÓN: Introduce otro dígito: "))
-        div = a/b
-        print(div)
+        try:
+            a = int(input("DIVISIÓN: Introduce dígito: "))
+            b = int(input("DIVISIÓN: Introduce otro dígito: "))
+        except ValueError:
+            print("Introduce un dígito, por favor. (Usted ha vuelto al menú.)")
+        else:
+            div = a/b
+            print(div)
     elif opcion == 5:
         print("Gracias por usar la calculadora.")
         break
